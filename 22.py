fbacf81e2d151c7f3f62d2f7c2f2d95c3be58ac2
Tom Huntington <55266932+tom-huntington@users.noreply.github.com>
2023-01-06 20:08:02 +1300

    refactor

--- 22.py
+++ 22.py
@@ -22,23 +22,48 @@ from sys import stdin
 
 # Part 2
 
-(S:=[*stdin.readlines()])
-(m:={i+1j*j:(c=='.') for i,s in enumerate(S[:-2]) for j,c in enumerate(s.rstrip()) if c in '.#'})
-(g:={})
-(n:=lambda x,d:[(x,-d*1j),(x-d*1j,d),(x+d*(1-1j),d*1j)][c(x,d)])
-(c:=lambda x,d:len({x-d*1j,x-d*1j+d}&m.keys()))
+RIGHT=0
+STRAIGHT=1
+REFLEX=2
 
-def f(x):
-    (y:=c(*x)==2 and n(*x) or (z:=f(n(*x))) and (z[0]+c(*x) and z[1] or f(z[1])[1]))
-    g.update({x:(y[0],-y[1]),y:(x[0],-x[1])})
-    return (c(*y),n(*y))
+S = [*stdin.readlines()]
+board = {complex(i,j):bool(c=='.') for i,s in enumerate(S[:-2]) for j,c in enumerate(s.rstrip()) if c in '.#'}
+transition = {}
+trav = lambda d: d*-1j # traversal direction
+nxt = lambda x,d:[(x,d*-1j), (x+trav(d),d), (x+trav(d)*(1j+1), d*1j)][angle(x,d)]
 
-f(next((x,d) for x in m for d in [1, -1, 1j, -1j] if x+d not in m and x+d*(1+1j) in m))
-advance = lambda x,d: g.get((x,d),(x+d,d))
-def fn(x,p):
-    if p in 'RL': return (x[0], x[1]*1j*(1-2*(p=='R')))
-    func = lambda curr,_: nxt if m[(nxt:=advance(*curr))[0]] else curr
-    return reduce(func, range(int(p)), x)
-(x:=reduce(fn,findall('\d+|R|L',S[-1]), (min(m,key=lambda x:(x.real,x.imag)),1j)))
+def angle(x,d):
+    match (x+trav(d) in board, # front
+           x+trav(d)*(1j+1) in board): # front-left
+        case True, True: return REFLEX
+        case (True, False)| \
+             (False, True): return STRAIGHT
+        case False, False: return RIGHT
+
+# d is exit direction, d*-1j is traversal direction
+
+def wormhole_(x,d):
+    if angle(x,d) == REFLEX:
+        return nxt(x,d) # base case. unwind recursions, zip edges
+    angle_enum, x_d = wormhole(*nxt(x,d))
+    if angle_enum == RIGHT and angle(x,d) == RIGHT: return wormhole(*x_d)[1] # stop ziping edges, queue recursions
+    return x_d # unwind recursions, zip edges
+
+def wormhole(x,d):
+    y = wormhole_(x,d)
+    transition.update({(x,d):(y[0],y[1]*-1),y:(x,d*-1)})
+    # return parents wormhole exit
+    return (angle(*y), nxt(*y))
+
+wormhole(*next((x,d) for x in board for d in [1, -1, 1j, -1j] if x+d not in board and x+d*(1+1j) in board))
+
+advance = lambda x,d: transition.get((x,d),(x+d,d))
+def fn(x,move):
+    if move in 'RL':
+        rotor = -1j if move == 'R' else 1j
+        return (x[0], x[1]*rotor)
+    func = lambda curr,_: nxt if board[(nxt:=advance(*curr))[0]] else curr
+    return reduce(func, range(int(move)), x)
+(x:=reduce(fn,findall('\d+|R|L',S[-1]), (min(board,key=lambda x:(x.real,x.imag)),1j)))
 print(1000*x[0].real+4*x[0].imag+1004+[1j,1,-1j,-1].index(x[1]))
 
